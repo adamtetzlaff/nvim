a900e00ff5f677df8f373caea1aee60ec29928f7
Adam Tetzlaff <adam.m.tetzlaff@gmail.com>
2023-12-17 20:25:45 -0600

    Make nvim-tree floating window

--- init.lua
+++ init.lua
@@ -15,6 +15,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local HEIGHT_RATIO = 0.8 -- You can change this
+local WIDTH_RATIO = 0.5 -- You can change this too
+
 require("lazy").setup({
 	{ "folke/which-key.nvim", event = "VeryLazy", opts = {} },
 	{ "lewis6991/gitsigns.nvim" },
@@ -237,8 +240,36 @@ require("lazy").setup({
 			"nvim-tree/nvim-web-devicons",
 		},
 		opts = {
+			disable_netrw = true,
+			hijack_netrw = true,
+			respect_buf_cwd = true,
+			sync_root_with_cwd = true,
 			view = {
-				width = 50,
+				relativenumber = true,
+				float = {
+					enable = true,
+					open_win_config = function()
+						local screen_w = vim.opt.columns:get()
+						local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+						local window_w = screen_w * WIDTH_RATIO
+						local window_h = screen_h * HEIGHT_RATIO
+						local window_w_int = math.floor(window_w)
+						local window_h_int = math.floor(window_h)
+						local center_x = (screen_w - window_w) / 2
+						local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+						return {
+							border = "rounded",
+							relative = "editor",
+							row = center_y,
+							col = center_x,
+							width = window_w_int,
+							height = window_h_int,
+						}
+					end,
+				},
+				width = function()
+					return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+				end,
 			},
 		},
 		config = function(_, opts)
